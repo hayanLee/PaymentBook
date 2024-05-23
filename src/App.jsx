6cdafc55bfb90e016af7bbf8c9f298007181773b
hayanLee <hayanlee.dev@gmail.com>
2024-05-24 01:49:09 +0900

    fix : ContextProvider 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import GlobalStyle from './components/GlobalStyle';
-import { PaymentProvider } from './context/PaymentContext';
 import store from './redux/store';
 import router from './routes/router';
 function App() {
     return (
         <>
             <GlobalStyle />
-            <PaymentProvider>
-                <Provider store={store}>
-                    <RouterProvider router={router} />
-                </Provider>
-            </PaymentProvider>
+            <Provider store={store}>
+                <RouterProvider router={router} />
+            </Provider>
         </>
     );
 }
